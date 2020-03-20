42c160e354612867aec4037edccc24a02a1aca23
almulalic <almir.mulalic.am@gmail.com>
2020-03-20 10:23:55 +0100

    zavrsen zadatak 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import java.util.*;
+import java.util.Arrays;
+
+
 public class Controller {
+    public Label outputArea;
+    public TextField textField;
+
+    private int SumirajCifre(int n) {
+        int suma = 0;
+        while(n!=0){
+            suma+=n%10;
+            n/=10;
+        }
+
+        return suma;
+    }
+
+    private ArrayList SumaCifara(int n) {
+        ArrayList lista = new ArrayList();
+
+        for(int i=1;i<n;i++) {
+            if(i%SumirajCifre(i) == 0)
+                lista.add(i);
+        }
+        
+        return lista;
+    }
+    public void ButtonAction(javafx.event.ActionEvent actionEvent) {
+        outputArea.setText(String.valueOf(SumaCifara(Integer.parseInt(textField.getText()))));
+    }
 }
